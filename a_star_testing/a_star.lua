cadbd05a55fdb5e781dc05f2901f161190de3e43
Bmorr1123 <33379673+Bmorr1123@users.noreply.github.com>
2022-04-14 16:12:31 -0500

    Adds Map Loading to a_star.lua

--- a_star_testing/a_star.lua
+++ a_star_testing/a_star.lua
@@ -1,5 +1,4 @@
 -- Data Management Area
-
 local world_data = {}
 
 function spotExists(fx, fy)
@@ -25,26 +24,46 @@ function enterSpot(fx, fy, value)
 
     world_data[fx][fy] = value
 
+    --print(world_data[fx][fy])
+
 end
 
-enterSpot(10, 10, "poop")
+function loadMap(map_path)
+    local file = fs.open(map_path, "r")
+
+    local y = 1
+    local line = file.readLine()
+    while line do
+        for x = 1, #line do
+            enterSpot(x, y, line:sub(x, x))
+        end
+
+        line = file.readLine()
+        y = y + 1
+    end
+end
 
 -- Actual Code
 function main()
+    term.clear()
     width, height = term.getSize()
 
     for y = 1, height do
+        term.setCursorPos(1, y)
         for x = 1, width do
             spot = getSpot(x, y)
-            if spot ~= "?" then
+
+            if spot ~= "?" and spot ~= " " then
                 term.setBackgroundColor(colors.white)
             else
                 term.setBackgroundColor(colors.black)
             end
             term.write(" ")
         end
-        term.setCursorPos(1, y)
+
     end
 end
 
+loadMap("map.txt")
 main()
+read()
